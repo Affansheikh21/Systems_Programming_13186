213ca47453d8c03d060a4dc6e6d3dff9b3ed9c14
Mohammad Affan Sheikh <affansheikh21@gmail.com>
2019-02-07 22:22:26 +0500

    Create Concatenate_pointers.c

---
+++ Concatenate_pointers.c
@@ -0,0 +1,13 @@
+void concatenate_string(char *original, char *add)
+{
+   while(*original)
+      original++;
+
+   while(*add)
+   {
+      *original = *add;
+      add++;
+      original++;
+   }
+   *original = '\0';
+}
