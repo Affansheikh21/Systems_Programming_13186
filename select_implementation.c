38badb1345c22e89d40dcdc16f735090e1e29e46
Mohammad Affan Sheikh <affansheikh21@gmail.com>
2019-05-15 22:42:40 +0500

    Monitor STDIN_FILENO; 5sec timer

---
+++ select_implementation.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+       #include <sys/stat.h>
+       #include <fcntl.h>
+#define MAX_SIZE 100
+
+int main() {
+
+struct timeval tv;
+fd_set fd;
+// timeout set is five seconds
+tv.tv_sec = 5;
+
+// populate set
+FD_ZERO(&fd);
+FD_SET(STDIN_FILENO,&fd);
+
+
+    int ret = select(STDIN_FILENO+1,&fd,NULL,NULL,&tv);
+        if (ret == -1){
+            perror("There's a problem");
+        }
+
+        else if (!ret) {
+            printf("Time elapsed: %d\n",tv.tv_sec);
+        }
+
+        if (__FD_ISSET(STDIN_FILENO,&fd) ) {
+            write(STDOUT_FILENO,"Data entered:\n",sizeof("Data entered:\n"));
+            char buff[MAX_SIZE];
+            int r = read(STDIN_FILENO,buff,100);
+            write(STDOUT_FILENO,buff,strlen(buff));
+            printf("Time remained: %d\n",tv.tv_sec);
+        }
+
+
+
+}
